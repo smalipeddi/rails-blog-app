741671681d01fb27e5bdbe8db7bcceaee46dbbe7
Sunitha Malipeddi <sunitha_malipeddi@yahoo.com>
2022-01-07 02:57:18 -0500

    add new and create actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,14 @@ class ArticlesController < ActionController::Base
 
     def show
         @article = Article.find(params[:id])
+    end
 
+    def new 
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        redirect_to @article
     end
 end
