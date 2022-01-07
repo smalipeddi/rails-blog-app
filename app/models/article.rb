29bf790ad23caaef5febc17bb286c31b03a30259
Sunitha Malipeddi <sunitha_malipeddi@yahoo.com>
2022-01-07 01:31:57 -0500

    add pages and articles controller, views

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
- validates :title, presence: true
- validates :description, presence: true
+ validates :title, presence: true , length: {minimum: 6}
+ validates :description, presence: true , length: { minimum: 6 , manimum: 250 }
 end
